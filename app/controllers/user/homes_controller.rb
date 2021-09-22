2d54a3b5b941b048c59fd96635a97d1458b258b6
taroken65 <yonemurano@gmail.com>
2021-09-22 07:18:51 +0000

    add top-layout

--- app/controllers/user/homes_controller.rb
+++ app/controllers/user/homes_controller.rb
@@ -1,6 +1,7 @@
 class User::HomesController < ApplicationController
     def top
         @topics = Topic.all.order(created_at: :desc).limit(10)
+        @categories = Category.all
         @view_ranks = Topic.find(ViewCount.group(:topic_id).order('count(topic_id) desc').limit(7).pluck(:topic_id))
         @favorite_ranks = Topic.find(Favorite.group(:topic_id).order('count(topic_id) desc').limit(7).pluck(:topic_id))
         @comment_ranks = Topic.find(Comment.group(:topic_id).order('count(topic_id) desc').limit(7).pluck(:topic_id))
